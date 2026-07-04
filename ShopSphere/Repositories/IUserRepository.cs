d06d4f7eb9a338ea3da3b7dba0242e25b93ea9ff
orestisLyk <orestis.lyk@outlook.com>
2026-07-04 18:50:26 +0300

    feat: add UserRepository

---
+++ ShopSphere/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShopSphere.Core;
+using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User?> GetUserByUsername(string username);
+        Task<PaginatedResult<User>> GetUsersAsync(int pageNumber, int pageSize);
+    }
+}
---
+++ ShopSphere/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSphere.Core;
+using ShopSphere.Data;
+using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(ShopSphereContext context) : base(context)
+        {
+        }
+        public async Task<User?> GetUserByUsername(string username)
+        {
+            return await context.Users
+                .Include(u => u.Role)
+                    .ThenInclude(r => r.Capabilities)
+                .FirstOrDefaultAsync(u => u.Username == username);
+        }
+        public async Task<PaginatedResult<User>> GetUsersAsync(int pageNumber, int pageSize)
+        {
+            var totalCount = await context.Users.CountAsync();
+
+            pageNumber = pageNumber < 1 ? 1 : pageNumber;
+            pageSize = pageSize < 1 ? 10 : pageSize;
+
+            var users = await context.Users
+                .Include(u => u.Role)
+                    .ThenInclude(r => r.Capabilities)
+                .OrderBy(u => u.Id)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return new PaginatedResult<User>
+            {
+                Items = users,
+                TotalRecords = totalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
+    }
+}
