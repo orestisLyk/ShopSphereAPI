fe19ad6f81128cf7800a604a6a76406d64379f4a
orestisLyk <orestis.lyk@outlook.com>
2026-07-04 19:47:29 +0300

    feat: add CategoryRepository

---
+++ ShopSphere/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSphere.Data;
+using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ShopSphereContext context) : base(context)
+        {
+        }
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await context.Categories.ToListAsync();
+        }
+        public async Task<Category?> GetCategoryByName(string name)
+        {
+            return await context.Categories.FirstOrDefaultAsync(c => c.Name == name);
+        }
+    }
+}
---
+++ ShopSphere/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public interface ICategoryRepository : IBaseRepository<Category>
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category?> GetCategoryByName(string name);
+    }
+}
