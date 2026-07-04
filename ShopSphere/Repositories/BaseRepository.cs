a096734ea872364358061ffe440774a4f0339991
orestisLyk <orestis.lyk@outlook.com>
2026-07-04 18:49:56 +0300

    feat: add BaseRepository class and interface as part of the repository design pattern

---
+++ ShopSphere/Repositories/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSphere.Data;
+
+namespace ShopSphere.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly ShopSphereContext context;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepository(ShopSphereContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+
+        public virtual async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await context.AddRangeAsync(entities);
+        }
+
+        public virtual Task UpdateAsync(T entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task<bool> DeleteAsync(int id)
+        {
+            T? existingEntity = await dbSet.FindAsync(id);
+            if (existingEntity is null) return false;
+            dbSet.Remove(existingEntity);
+            return true;
+        }
+
+        public virtual async Task<T?> GetAsync(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public virtual async Task<int> GetCountAsync()
+        {
+            return await dbSet.CountAsync();
+        }
+    }
+}
