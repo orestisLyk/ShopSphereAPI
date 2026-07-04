355cf83f646452f9c0167ab1b06582a0f0e85d93
orestisLyk <orestis.lyk@outlook.com>
2026-07-04 18:48:31 +0300

    feat: add Encryption utility class and interface

---
+++ ShopSphere/Security/EncryptionUtil.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopSphere.Security
+{
+    public class EncryptionUtil : IEncryptionUtil
+    {
+        public string Encrypt(string plainText)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(plainText);
+        }
+
+        public bool Verify(string plainText, string hashedText)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainText, hashedText);
+        }
+    }
+}
---
+++ ShopSphere/Security/IEncryptionUtil.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopSphere.Security
+{
+    public interface IEncryptionUtil
+    {
+        string Encrypt(string plainText);
+        bool Verify(string plainText, string hashedText);
+    }
+}
