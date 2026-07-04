0bf9cca1214b27828c7cb367dfe2af5e1b2dadae
orestisLyk <orestis.lyk@outlook.com>
2026-07-04 18:47:50 +0300

    feat: add PaginatedResult class

---
+++ ShopSphere/Core/PaginatedResult.cs
@@ -0,0 +1,21 @@
+﻿namespace ShopSphere.Core
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Items { get; set; } = [];
+        public int TotalRecords { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalRecords / PageSize);
+
+        public PaginatedResult() { }
+
+        public PaginatedResult(List<T> items, int totalRecords, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalRecords = totalRecords;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
