8a11f6193d71d2ecce851d88a59d50fd552ae5a1
orestisLyk <orestis.lyk@outlook.com>
2026-07-05 15:30:21 +0300

    feat: add UnitOfWork class and interface

---
+++ ShopSphere/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopSphere.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        IProductRepository ProductRepository { get; }
+        IProductImageRepository ProductImageRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+        ICartRepository CartRepository { get; }
+        IOrderRepository OrderRepository { get; }
+        IPaymentRepository PaymentRepository { get; }
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ ShopSphere/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using ShopSphere.Data;
+
+namespace ShopSphere.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ShopSphereContext context;
+
+        public IUserRepository UserRepository { get; }
+        public IProductRepository ProductRepository { get; }
+        public IProductImageRepository ProductImageRepository { get; }
+        public ICategoryRepository CategoryRepository { get; }
+        public ICartRepository CartRepository { get; }
+        public IOrderRepository OrderRepository { get; }
+        public IPaymentRepository PaymentRepository { get; }
+
+        public UnitOfWork(ShopSphereContext context)
+        {
+            this.context = context;
+            UserRepository = new UserRepository(context);
+            ProductRepository = new ProductRepository(context);
+            ProductImageRepository = new ProductImageRepository(context);
+            CategoryRepository = new CategoryRepository(context);
+            CartRepository = new CartRepository(context);
+            OrderRepository = new OrderRepository(context);
+            PaymentRepository = new PaymentRepository(context);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
