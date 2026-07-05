f28aaf3355d816d6f79932e92fef89f933de7989
orestisLyk <orestis.lyk@outlook.com>
2026-07-05 13:11:20 +0300

    chore: remove openApi dependency

--- ShopSphere/Program.cs
+++ ShopSphere/Program.cs
@@ -22,16 +22,11 @@ namespace ShopSphere
             builder.Services.AddSingleton<IEncryptionUtil, EncryptionUtil>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
 
             app.UseHttpsRedirection();
 
