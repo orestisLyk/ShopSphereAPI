6b9f863ad0bcc4fc9581ae804332f8d8a9eb1798
orestisLyk <orestis.lyk@outlook.com>
2026-07-05 13:54:52 +0300

    feat: add PaymentRepository

---
+++ ShopSphere/Repositories/IPaymentRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public interface IPaymentRepository : IBaseRepository<Payment>
+    {
+        Task<Payment?> GetPaymentByOrderIdAsync(int orderId);
+        Task<bool> PaymentExistsForOrderAsync(int orderId);
+    }
+}
---
+++ ShopSphere/Repositories/PaymentRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSphere.Data;
+using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public class PaymentRepository : BaseRepository<Payment>, IPaymentRepository
+    {
+        public PaymentRepository(ShopSphereContext context) : base(context)
+        {
+        }
+
+        public async Task<Payment?> GetPaymentByOrderIdAsync(int orderId)
+        {
+            return await context.Payments.FirstOrDefaultAsync(p => p.Order.Id == orderId);
+        }
+
+        public async Task<bool> PaymentExistsForOrderAsync(int orderId)
+        {
+            return await context.Payments.AnyAsync(p => p.Order.Id == orderId);
+        }
+    }
+}
