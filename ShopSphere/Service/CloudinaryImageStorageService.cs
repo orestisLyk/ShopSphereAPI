4f6d9a13fdb337d1259a8da5760657dc3b33016b
orestisLyk <orestis.lyk@outlook.com>
2026-07-05 15:32:28 +0300

    feat: add Generic IImageStorageService interface and Cloudinary implementation

---
+++ ShopSphere/Service/CloudinaryImageStorageService.cs
@@ -0,0 +1,36 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using ShopSphere.Exceptions;
+
+namespace ShopSphere.Service
+{
+    public class CloudinaryImageStorageService : IImageStorageService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryImageStorageService(Cloudinary cloudinary)
+        {
+            _cloudinary = cloudinary;
+        }
+
+        public async Task<string> UploadImageAsync(int houseId, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentException("File is null or empty", nameof(file));
+            }
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                PublicId = $"house_{houseId}/{Guid.NewGuid()}", // Unique identifier for the image
+                Overwrite = true
+            };
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            if (uploadResult.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new ImageUploadException($"Image upload failed: {uploadResult.Error?.Message}");
+            }
+            return uploadResult.SecureUrl.ToString();
+        }
+    }
+}
---
+++ ShopSphere/Service/IImageStorageService.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopSphere.Service
+{
+    public interface IImageStorageService
+    {
+        Task<string> UploadImageAsync(
+            int houseId,
+            IFormFile file
+            );
+    }
+}
