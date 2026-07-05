b734335c60e11cc51ade5a8150b7b698178212cc
orestisLyk <orestis.lyk@outlook.com>
2026-07-05 15:29:38 +0300

    feat: add ProductImageRepository

---
+++ ShopSphere/Repositories/IProductImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public interface IProductImageRepository : IBaseRepository<ProductImage>
+    {
+        Task<IEnumerable<ProductImage>> GetImagesByProductIdAsync(int productId);
+        Task<ProductImage?> GetImageByIdAsync(int id);
+        Task<bool> ImageExistsForProductAsync(int productId);
+    }
+}
---
+++ ShopSphere/Repositories/ProductImageRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSphere.Data;
+using ShopSphere.Model;
+
+namespace ShopSphere.Repositories
+{
+    public class ProductImageRepository : BaseRepository<ProductImage>, IProductImageRepository
+    {
+        public ProductImageRepository(ShopSphereContext context) : base(context) { }
+
+        public async Task<IEnumerable<ProductImage>> GetImagesByProductIdAsync(int productId)
+        {
+            return await context.ProductImages
+                .Where(pi => pi.ProductId == productId)
+                .ToListAsync();
+        }
+
+        public async Task<ProductImage?> GetImageByIdAsync(int id)
+        {
+            return await context.ProductImages
+                .FirstOrDefaultAsync(pi => pi.Id == id);
+        }
+
+        public async Task<bool> ImageExistsForProductAsync(int productId)
+        {
+            return await context.ProductImages
+                .AnyAsync(pi => pi.ProductId == productId);
+        }
+    }
+}
