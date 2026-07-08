ce99f0719b82b13ca6b7d655d1bd9dcc300a793b
orestisLyk <orestis.lyk@outlook.com>
2026-07-08 16:04:29 +0300

    feat: add EntityNotFound Exception

---
+++ ShopSphere/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopSphere.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
