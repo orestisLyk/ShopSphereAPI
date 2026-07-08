d1002edec0ec0840675239560c878b6dcf31ea76
orestisLyk <orestis.lyk@outlook.com>
2026-07-08 16:07:57 +0300

    feat: add Mapper Profile and map added DTOs

---
+++ ShopSphere/Configuration/MapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ShopSphere.DTO;
+using ShopSphere.Model;
+
+namespace ShopSphere.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Product, ProductMinimalDTO>();
+            CreateMap<Product, ProductDetailsDTO>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
+                .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.ProductImages.Select(pi => pi.ImageUrl).ToList()));
+            CreateMap<ProductCreateDTO, Product>();
+        }
+    }
+}
