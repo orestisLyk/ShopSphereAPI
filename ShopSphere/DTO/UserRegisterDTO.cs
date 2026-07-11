a8f626644b3d8fc9d55d89b13fd05784d683b499
orestisLyk <orestis.lyk@outlook.com>
2026-07-11 11:51:50 +0300

    feat: add user dtos

---
+++ ShopSphere/DTO/UserRegisterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopSphere.DTO
+{
+    public record UserRegisterDTO(
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username cannot exceed 50 characters.")]
+        string Username,
+        [Required(ErrorMessage = "First name is required.")]
+        string FirstName,
+        [Required(ErrorMessage = "Last name is required.")]
+        string LastName,
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        string Email,
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        string Password,
+        [Required(ErrorMessage = "Role ID is required.")]
+        int RoleId
+    )
+    {
+    }
+}
---
+++ ShopSphere/DTO/UserUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopSphere.DTO
+{
+    public record UserUpdateDTO(
+        [StringLength(50, ErrorMessage = "Username cannot exceed 50 characters.")]
+        string? Username,
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        string? Email,
+        string? OldPassword,
+        [MinLength(6, ErrorMessage = "New password must be at least 6 characters long.")]
+        string? NewPassword
+    )
+    {
+    }
+}
