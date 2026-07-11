da05edadbbcf21d092c6b4ec34e65e8f2b19845d
orestisLyk <orestis.lyk@outlook.com>
2026-07-11 13:03:34 +0300

    feat: configure jwt authentication, cors and swagger

--- ShopSphere/Program.cs
+++ ShopSphere/Program.cs
@@ -1,11 +1,15 @@
 
 using CloudinaryDotNet;
 using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ShopSphere.Data;
+using ShopSphere.Helpers;
 using ShopSphere.Repositories;
 using ShopSphere.Security;
 using ShopSphere.Service;
+using System.Reflection;
 
 namespace ShopSphere
 {
@@ -25,6 +29,71 @@ namespace ShopSphere
             // Get the connection string from environment variables
             var connString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 
+            //Configure JWT authentication
+            var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.IncludeErrorDetails = builder.Environment.IsDevelopment();
+                options.SaveToken = true;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtSettings["Key"]!))
+                };
+            });
+
+            //CORS configuration
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowClient",
+                    b => b.WithOrigins(builder.Configuration["Cors:Origin"]!)
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+            });
+
+            //JSON Serialization Configuration
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+                    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+                    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+                });
+
+            //Swagger Configuration
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "ShopSphere API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+                    new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = JwtBearerDefaults.AuthenticationScheme,
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT Authorization header using the Bearer scheme."
+                    });
+                options.OperationFilter<AuthorizeOperationFilter>();
+
+            });
 
             // Add services to the container.
 
@@ -43,10 +112,18 @@ namespace ShopSphere
 
             var app = builder.Build();
 
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
 
+            app.UseCors("AllowClient");
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
