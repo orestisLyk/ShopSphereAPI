502e60e4b6e6622ab2a67fb81283a81b5ea07fed
orestisLyk <orestis.lyk@outlook.com>
2026-07-11 13:03:00 +0300

    feat: add SwaggerGen filter

---
+++ ShopSphere/Helpers/AuthorizeOperationFilter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ShopSphere.Helpers
+{
+    public class AuthorizeOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var authAttributes = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<Microsoft.AspNetCore.Authorization.AuthorizeAttribute>()
+                .Distinct();
+
+            if (authAttributes.Any())
+            {
+                // Use indexer to avoid duplicate key exceptions
+                operation.Responses["401"] = new OpenApiResponse { Description = "Unauthorized" };
+                operation.Responses["403"] = new OpenApiResponse { Description = "Forbidden" };
+
+                // Add security requirements
+                operation.Security = new List<OpenApiSecurityRequirement>();
+
+                var roles = context.MethodInfo.GetCustomAttributes(true)
+                    .OfType<AuthorizeAttribute>()
+                    .Where(attr => !string.IsNullOrEmpty(attr.Roles))
+                    .SelectMany(attr => attr.Roles!.Split(','))
+                    .Select(r => r.Trim());
+
+                // Add the security requirement for the JWT Bearer scheme with specified roles
+                operation.Security.Add(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Description = "Add token to header",
+                            Name = "Authorization",
+                            Type = SecuritySchemeType.Http,
+                            In = ParameterLocation.Header,
+                            Scheme = JwtBearerDefaults.AuthenticationScheme,
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        roles.ToList()
+                    }
+                });
+
+
+            }
+        }
+    }
+}
