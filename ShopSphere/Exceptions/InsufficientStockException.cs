c913aa192fb6153e44fb96f263e0f2346f96ab8b
orestisLyk <orestis.lyk@outlook.com>
2026-07-12 16:15:37 +0300

    feat: add insufficientstock exception

---
+++ ShopSphere/Exceptions/InsufficientStockException.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopSphere.Exceptions
+{
+    public class InsufficientStockException : Exception
+    {
+        public InsufficientStockException(string message) : base(message)
+        {
+        }
+    }
+}
