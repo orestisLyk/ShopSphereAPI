c5f1662e85ad4425ae95e2724e3e8b34a285098b
orestisLyk <orestis.lyk@outlook.com>
2026-07-14 15:33:38 +0300

    feat: add order readonly dto

---
+++ ShopSphere/DTO/OrderReadOnlyDTO.cs
@@ -0,0 +1,13 @@
+﻿using ShopSphere.Enums;
+
+namespace ShopSphere.DTO
+{
+    public record OrderReadOnlyDTO(
+        int Id,
+        decimal TotalAmount,
+        OrderStatus Status,
+        string Username
+        )
+    {
+    }
+}
